fa49157df869e90854e0d9f9d09b4123bf818e3c
Mohammadalsadi19 <160345239+Mohammadalsadi19@users.noreply.github.com>
2024-02-21 18:59:25 +0300

    Add files via upload

--- bank-master/bank/main.cpp
+++ bank-master/bank/main.cpp
@@ -1,111 +1,160 @@
-    #include <iostream>
-    #include <string>
-    #include <fstream>
-    #include <algorithm>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <vector>
 
-    using namespace std;
+using namespace std;
 
-    class User {
-    private:
-        string UID;
-        string name;
-        enum card { silver, gold, diamond };
-        bool gender;
-        float balance;
-        float normalBalance;
-        string password;
+class User {
+private:
+    string UID;
+    string name;
+    string gender;
+    string balance;
+    string password;
 
-    public:
-        string fetchData() {
-            ifstream get("data.txt");
-            User user[10];
-            string data;
-            while (getline(get, data)) {
-                cout << data << endl;
+public:
+    User()
+        : UID(""), name(""), gender(""), password(""), balance("") {}
+    User(const string& UID, const string& name, const string& gender,
+        const string& password, const string& balance)
+        : UID(UID), name(name), gender(gender), password(password), balance(balance) {}
+
+    void fetchData() {
+        ifstream get("data.txt");
+        string data;
+        while (get >> UID >> name >> gender >> password >> balance) {
+            //instanceOfUser.emplace_back();
+            cout << "UID: " << UID << ", Name: " << name << ", Gender: " << gender << ", Balance: " << balance << endl;
+            //cout << instanceOfUser.back().UID << instanceOfUser.back().name << instanceOfUser.back().password << endl;
+        }
+    }
+    void setData(string addedData) {
+        ofstream set("data.txt", ios::app);
+        set << addedData << endl;
+    }
+
+    bool checkCredentials(const string& EUID, const string& Epassword) const {
+        return (EUID == UID && Epassword == password);
+    }
+
+    bool create() {
+        UID = to_string(rand() % 90000 + 9999);
+        cout << "Enter your name: ";
+        cin >> name;
+        cout << "If you are male, enter 1. If you are female, enter 0: ";
+        cin >> gender;
+        cout << "Set Your Password: ";
+        cin >> password;
+        cout << "Enter your initial balance : ";
+        cin >> balance;
+        string data = UID + " " + name + " " + gender + " " + password + " " + balance;
+        setData(data);
+        return true;
+    }
+    bool withdraw(float amount) {
+        float sbalance = stof(balance);
+        if (amount <= stof(balance)) {
+            if ((int)amount % 5 == 0) {
+                cout << "Pull your card please" << endl;
+                sbalance -= amount;
+                balance = to_string(sbalance);
+                return true;
             }
-            return data;
         }
-        void setData(string addedData) {
-            ofstream set("data.txt");
-            set << addedData << endl;
-        }
-        User() {
-            fetchData();
-            bool acc;
-            cout << "Do You Have an account: (0/1)";
-            cin >> acc;
-            if (acc) {
-                login();
-            }
-            else {
-                create();
-            }
+        return false;
+    }
 
-        }
-        bool checkCredentials(string EUID, string Epassword) {
-            cout << "Debug: EUID=" << EUID << " Epassword=" << Epassword
-                << " UID=" << UID << " password=" << password << endl;
-            return (EUID == UID && Epassword == password);
-        }
+    void deposit(float amount) {
+        float sbalance = stof(balance);
+        cout << "Pull your card please" << endl;
+        sbalance += amount;
+        balance = to_string(sbalance);
+    }
 
+    string checkBalance() {
+        return balance;
+    }
+};
 
-        bool create() {
-            string data;
-            UID = to_string(rand() % 90000 + 9999);
-            cout << "Enter your name: ";
-            cin >> name;
-            cout << "if u r male enter 1, if u r female enter 0: ";
-            cin >> gender;
-            cout << "Set Your Password: ";
-            cin >> password;
-            cout << "enter your initial balance : ";
-            cin >> balance;
-            data = UID + " " + name + " " + to_string(gender) + " " + password + " " + to_string(balance);
-            setData(data);
-            return 1;
-        }
+int main() {
+    srand(time(0));
+    vector<User> users;
 
+    ifstream get("data.txt");
+    string UID, name, gender, password, balance;
+    while (get >> UID >> name >> gender >> password >> balance) {
+        users.emplace_back(UID, name, gender, password, balance);
+        cout << "UID: " << UID << ", Name: " << name << ", Gender: " << gender << ", Balance: " << balance << endl;
+    }
+    get.close();
 
-        bool login() {
-            string EUID;
-            string Epassword;
-            cout << "Enter Your ID";
+    bool acc;
+    User currentUser;  // Store the current user once logged in
+
+    while (true) {
+        cout << "Do You Have an account: (0/1)";
+        cin >> acc;
+
+        if (acc) {
+            string EUID, Epassword;
+            cout << "Enter Your ID: ";
             cin >> EUID;
-            cout << "Enter Your Password";
+            cout << "Enter Your Password: ";
             cin >> Epassword;
-            if (checkCredentials(EUID, Epassword)) {
-                cout << "Your Are Logged In";
-                return 1;
-            }
-            else {
-                cout << "Try Again!" << endl;
-                login();
-            }
-        }
 
-        bool withdraw(float amount) {
-            if (amount <= balance) {
-                if ((int)amount % 5 == 0) {
-                    cout << "pull your card please";
-                    balance -= amount;
-                    return 1;
+            bool loginSuccessful = false;
+            for (auto& user : users) {
+                if (user.checkCredentials(EUID, Epassword)) {
+                    loginSuccessful = true;
+                    cout << "Login successful!" << endl;
+                    currentUser = user;  // Store the current user for further actions
+                    break;
                 }
             }
-            else return 0;
-        }
-        void deposit(float amount) {
-            cout << "pull your card please";
-            balance += amount;
-        }
-        float checkBalance() {
-            return balance;
-        }
-    };
 
+            if (!loginSuccessful) {
+                cout << "Login failed. Invalid UID or password." << endl;
+            }
+            else {
+                // Once logged in, provide a menu for actions
+                int choice;
+                while (true) {
+                    cout << "1. Check Balance\n2. Deposit\n3. Logout\n4. Withdraw\nEnter your choice: ";
+                    cin >> choice;
 
-    int main()
-    {
-        srand(time(0));
-        User user[50];
-        return 0;
+                    switch (choice) {
+                    case 1:
+                        cout << "Balance: " << currentUser.checkBalance() << endl;
+                        break;
+                    case 2:
+                        float amount;
+                        cout << "Enter the amount to deposit: ";
+                        cin >> amount;
+                        currentUser.deposit(amount);
+                        cout << "Deposit successful. New balance: " << currentUser.checkBalance() << endl;
+                        break;
+                    case 4:
+                        float amountw;                        
+                        cout << "Enter the amount to withdraw: ";
+                        cin >> amountw;
+                        currentUser.withdraw(amountw);
+                        break;
+                    case 3:
+                        // Logout and exit the inner loop
+                        break;
+                    default:
+                        cout << "Invalid choice. Please try again." << endl;
+                    }
+
+                    if (choice == 3) {
+                        break;  // Exit the inner loop when user chooses to logout
+                    }
+                }
+            }
+        }
     }
+
+    return 0;
+}
